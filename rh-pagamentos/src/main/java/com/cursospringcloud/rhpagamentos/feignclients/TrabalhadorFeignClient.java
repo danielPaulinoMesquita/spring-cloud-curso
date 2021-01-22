55eb87fd723b79e3ee2180c011e2adc72dc0af8a
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2021-01-21 23:15:25 -0300

    5- Usando Netflix Ribbon para balanceamento de carga, subindo duas instâncias da aplicação rh-trabalhador e consumindo-as

--- rh-pagamentos/src/main/java/com/cursospringcloud/rhpagamentos/feignclients/TrabalhadorFeignClient.java
+++ rh-pagamentos/src/main/java/com/cursospringcloud/rhpagamentos/feignclients/TrabalhadorFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(value = "rh-trabalhador", url = "localhost:8001", path = "/trabalhadores")
+@FeignClient(value = "rh-trabalhador", path = "/trabalhadores")
 public interface TrabalhadorFeignClient {
 
     @GetMapping(value = "/{id}")
