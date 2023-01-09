e038f3504fae71ec26744e75ce1492ce0f5b7333
Daniel Paulino <daniel-paulino@hotmail.com>
2023-01-09 00:48:12 -0300

    6.2 Configuração inicial para EurekaServer.

--- rh-eurekaserver/src/main/java/com/cursospringcloud/rheurekaserver/RhEurekaserverApplication.java
+++ rh-eurekaserver/src/main/java/com/cursospringcloud/rheurekaserver/RhEurekaserverApplication.java
@@ -2,10 +2,12 @@ package com.cursospringcloud.rheurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class RhEurekaserverApplication {
-
+    // TODO PODE VISUALIZAR O EUREKA SERVER ACESSANDO A PORTA DELE QUE É http://localhost:8761
     public static void main(String[] args) {
         SpringApplication.run(RhEurekaserverApplication.class, args);
     }
