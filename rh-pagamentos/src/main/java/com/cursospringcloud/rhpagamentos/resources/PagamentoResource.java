31996fef6d1c6164c7528cddc3317b6d747e6cfc
Daniel Paulino <daniel-paulino@hotmail.com>
2023-01-15 02:24:36 -0300

    6.4 Configuração Hystrix para tolerância a falhas

--- rh-pagamentos/src/main/java/com/cursospringcloud/rhpagamentos/resources/PagamentoResource.java
+++ rh-pagamentos/src/main/java/com/cursospringcloud/rhpagamentos/resources/PagamentoResource.java
@@ -2,6 +2,8 @@ package com.cursospringcloud.rhpagamentos.resources;
 
 import com.cursospringcloud.rhpagamentos.entities.Pagamento;
 import com.cursospringcloud.rhpagamentos.services.PagamentoService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,16 +18,41 @@ public class PagamentoResource {
     @Autowired
     private PagamentoService service;
 
+
+    @HystrixCommand(fallbackMethod = "fallback_teste",
+            commandProperties = {
+                    @HystrixProperty(
+                            name = "execution.isolation.thread.timeoutInMilliseconds",
+                            value = "3000")
+            }
+    )
     @GetMapping(value = "/{idTrabalhador}/dias/{dias}")
     public ResponseEntity<Pagamento> getPagamento(@PathVariable long idTrabalhador, @PathVariable Integer dias){
         Pagamento pagamento = service.getPagamento(idTrabalhador, dias);
         return ResponseEntity.ok(pagamento);
     }
 
+    @HystrixCommand(fallbackMethod = "fallback_teste",
+            commandProperties = {
+                    @HystrixProperty(
+                            name = "execution.isolation.thread.timeoutInMilliseconds",
+                            value = "3000")
+            }
+    )
     @GetMapping(value = "/tst")
-    public ResponseEntity<?> getPagamento(){
+    public ResponseEntity<?> getPagamento() throws InterruptedException{
+        Thread.sleep(2500);
         String resp = "TESTANDO";
         return ResponseEntity.ok(resp);
+    }
+
+    private ResponseEntity<?>  fallback_teste() {
+        return ResponseEntity.ok("testando o hystrix e vendo como funciona");
+    }
+
+    private ResponseEntity<Pagamento>  fallback_teste(long idTrabalhador, Integer dias) {
+        Pagamento pagamento = new Pagamento("Teste", 2000.00, 5);
+        return ResponseEntity.ok(pagamento);
     }
 
 }
