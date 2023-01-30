63b7b99a40d93c7dac73fe7f59b0ae8de6952158
Daniel Paulino <daniel-paulino@hotmail.com>
2023-01-30 17:50:56 -0300

    7.3.0 Alterado (repository private) forma de logar github e pegar as configs no repo spring config

--- rh-config-server/README.md
+++ rh-config-server/README.md
@@ -6,3 +6,10 @@
 
 In the following url:
 https://github.com/danielPaulinoMesquita/curso-springcloud-config/tree/main
+
+Now, that repository is private to avoid configs and enviorements, to discovers and sees the configs it was necessary declare 
+new values from spring cloud git in properties. like:
+spring.cloud.config.server.git.username=${GITHUB_USER}
+spring.cloud.config.server.git.password=${GITHUB_PRIVATE_KEY}
+
+these ${} are variables declared in IDE or could be declared in envs at server.
--- rh-config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ rh-config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -1,12 +1,25 @@
 package com.example.configserver;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigServerApplication implements CommandLineRunner {
+
+//    @Value("${spring.cloud.config.server.git.username}")
+//    private String userGit;
+//    @Value("${spring.cloud.config.server.git.password}")
+//    private String userPass;
+
+    //Serve para passar alguma instrução durante a execução do Spring
+    @Override
+    public void run(String... args) throws Exception {
+//        System.out.println("USERNAME == "+userGit); verifica se as váriveis esão sendo testadas.
+//        System.out.println("PASSWORD == "+userPass);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
