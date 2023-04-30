f9d5bb8bd88f5b857c94181f0ff6601063c1e7a9
Daniel Paulino <daniel-paulino@hotmail.com>
2023-04-29 23:21:18 -0300

    8.0.1 commitando o que faltava do rh-usuario

---
+++ rh-usuario/src/main/java/com/example/rhusuario/RhUsuarioApplication.java
@@ -0,0 +1,15 @@
+package com.example.rhusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class RhUsuarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhUsuarioApplication.class, args);
+    }
+
+}
