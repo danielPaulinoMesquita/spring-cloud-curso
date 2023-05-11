48790b113962493937c7d745d2c471519412fbe7
Daniel Paulino <daniel-paulino@hotmail.com>
2023-05-11 01:29:27 -0300

    8.7.0 Login with JWT token Auth

--- rh-usuario/src/main/resources/data.sql
+++ rh-usuario/src/main/resources/data.sql
@@ -1,4 +1,6 @@
-INSERT INTO tb_user (name,email,password) values ('Nina Brown', 'nina@gmail.com','TmluYSBCcm93bg==');
+INSERT INTO tb_user (name,email,password) values ('Nina Brown',
+                                                  'nina@gmail.com',
+                                                '$2a$12$PcaigDTTynIcSEcJQ8obqe1Wxo5RltmYPa5VDvXZy1xvxuSHhbtlS');
 INSERT INTO tb_user (name,email,password) values ('Leia Red', 'leia@gmail.com','TGVpYSBSZWQ=');
 
 INSERT INTO tb_role (role_name) values ('ROLE_OPERATOR');
